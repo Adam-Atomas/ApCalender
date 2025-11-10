85e1117807a5d43055fc40e0ba5912dbd630e852
AdamShi <adams221@nycstudents.net>
2025-11-10 12:29:56 -0500

    new

--- src/ApCalender.java
+++ src/ApCalender.java
@@ -14,25 +14,31 @@ public class ApCalender {
     }
     private static int firstDayOfYear ( int year){
         int day = 1;
-        if(int i = 1; i < year; i ++){
+        if(int i = 1; i < year; i++){
             day++;
             if (isLeapYear(i)) day++;
         }
         return day % 7;
     }
-    private static int dayOfYear (int month, int day, int year){
+    private static int dayOfYear (int month, int day, int year) {
         int days = 0;
-        if (month >1) days += 31;
-        if (month >2) days += 28;
-        if (month >3) days += 31;
-        if (month >4) days += 31;
-        if (month >) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
-        if (month >1) days += 31;
+        if (isLeapYear(year)) days++;
+        if (month > 1) days += 31;
+        if (month > 2) days += 28;
+        if (month > 3) days += 31;
+        if (month > 4) days += 30;
+        if (month > 5) days += 31;
+        if (month > 6) days += 30;
+        if (month > 7) days += 31;
+        if (month > 8) days += 31;
+        if (month > 9) days += 30;
+        if (month > 10) days += 31;
+        if (month > 11) days += 30;
+        days += day;
+        return days;
+    }
+    private static int dayOfWeek (int month, int day, int year){
+    int days = dayOfYear(month, day, year)%7;
+    return firstDayOfYear(year)+days-1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(ApCalender.numberOfLeapYears(2000, 2020));
+        System.out.println(ApCalender.dayOfWeek(1, 5, 2019));
+        System.out.println(ApCalender.dayOfWeek(1, 10, 2019);
     }
 
 }
